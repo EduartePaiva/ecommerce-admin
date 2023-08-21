01df72eea73a1af3d4f6ddeff31d29a36fe410a1
Eduarte Paiva de Oliveira <74308809+EduartePaiva@users.noreply.github.com>
2023-08-21 14:02:45 -0300

    FInal
    Versão final

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import MainNav from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "./theme-toggle";
 
 export default async function Navbar() {
     const userId = auth().userId
@@ -20,6 +21,7 @@ export default async function Navbar() {
                 <StoreSwitcher items={stores} />
                 <MainNav className="ms-6" />
                 <div className="ml-auto flex items-center space-x-4">
+                    <ThemeToggle />
                     <UserButton afterSignOutUrl="/" />
                 </div>
             </div>
